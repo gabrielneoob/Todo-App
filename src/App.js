f2a503b3c23160c8390757c2eed883bc5b053f89
Neoob <gabrielngc@gmail.com>
2022-11-05 14:55:18 -0300

    form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 
 function App() {
   const [todo, setTodo] = useState([]);
+  const LOCAL_STORAGE_KEY = 'todo.app';
+
+  useEffect(() => {
+    console.log(todo);
+  }, [todo])
 
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form todo={todo} setTodo={setTodo} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const Form = () => {
+const Form = ({ todo, setTodo }) => {
+  const inputText = useRef();
+  const [notificacao, setNotificacao] = useState(null);
+
+  function handleAddTodo(e) {
+    e.preventDefault();
+
+    const texto = inputText.current.value;
+    const randomId = Math.floor(Math.random() * 9000);
+
+    if (texto && !todo.some((todo) => todo.todo === texto)) {
+      setTodo([...todo, { id: randomId, todo: texto, completed: false }])
+    } else {
+      setNotificacao(<span style={{ position: 'fixed', top: '0', bottom: '0', right: '0', left: '0', backgroundColor: 'rgba(0,0,0, 0.1)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Todo já adicionado!</span>)
+      setTimeout(() => {
+        setNotificacao(null);
+      }, 2000)
+    }
+    inputText.current.value = null
+  }
   return (
     <form>
-      <input type="text" className="todo-input" />
-      <button type="submit" className="todo-button"><i class="fas fa-plus-square"></i></button>
+      <input ref={inputText} type="text" className="todo-input" />
+      {notificacao ? notificacao : null}
+      <button onClick={handleAddTodo} type="submit" className="todo-button"><i className="fas fa-plus-square"></i></button>
       <div className="select">
         <select name="todos" className="filter-todo">
           <option value="">All</option>
@@ -12,7 +32,7 @@ const Form = () => {
           <option value="">uncompleted</option>
         </select>
       </div>
-    </form>
+    </form >
   );
 }
 
